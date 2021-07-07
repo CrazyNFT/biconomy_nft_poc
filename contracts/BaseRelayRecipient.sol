346d8093536bec1799d90a6cf106dcfe34c99426
IshaanSrivastavaaa <48021627+IshaanSrivastavaaa@users.noreply.github.com>
2021-07-07 12:34:52 +0530

    Create BaseRelayRecipient.sol

---
+++ contracts/BaseRelayRecipient.sol
@@ -0,0 +1,38 @@
+pragma solidity ^0.8.0;
+
+import "irelayrecipient.sol";
+
+/**
+ * A base contract to be inherited by any contract that want to receive relayed transactions
+ * A subclass must use "_msgSender()" instead of "msg.sender"
+ */
+abstract contract BaseRelayRecipient is IRelayRecipient {
+
+    /*
+     * Forwarder singleton we accept calls from
+     */
+    address public trustedForwarder;
+
+    /*
+     * require a function to be called through GSN only
+     */
+    modifier trustedForwarderOnly() {
+        require(msg.sender == address(trustedForwarder), "Function can only be called through the trusted Forwarder");
+        _;
+    }
+
+    function isTrustedForwarder(address forwarder) public override view returns(bool) {
+        return forwarder == trustedForwarder;
+    }
+
+    function _msgSender() internal view virtual override returns (address sender) {
+        if (isTrustedForwarder(msg.sender)) {
+            assembly { 
+                sender := shr(96, calldataload(sub(calldatasize(), 20))) 
+            }
+        } else {
+            return _msgSender();
+        }
+    }
+    
+}
