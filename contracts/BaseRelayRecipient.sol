8cf8980e04c9bb528cd40a48c6dde6f9019dcdad
IshaanSrivastavaaa <48021627+IshaanSrivastavaaa@users.noreply.github.com>
2021-07-12 17:28:06 +0530

    Update BaseRelayRecipient.sol

--- contracts/BaseRelayRecipient.sol
+++ contracts/BaseRelayRecipient.sol
@@ -1,6 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0-only
+// solhint-disable no-inline-assembly
 pragma solidity ^0.8.0;
 
-import "https://github.com/CrazyNFT/biconomy_nft_poc/blob/master/contracts/IRelayRecipient.sol";
+import "IRelayRecipient.sol";
 
 /**
  * A base contract to be inherited by any contract that want to receive relayed transactions
@@ -11,28 +13,44 @@ abstract contract BaseRelayRecipient is IRelayRecipient {
     /*
      * Forwarder singleton we accept calls from
      */
-    address public trustedForwarder;
-
-    /*
-     * require a function to be called through GSN only
-     */
-    modifier trustedForwarderOnly() {
-        require(msg.sender == address(trustedForwarder), "Function can only be called through the trusted Forwarder");
-        _;
-    }
+    address public trustedForwarder = address(0xFD4973FeB2031D4409fB57afEE5dF2051b171104);
 
     function isTrustedForwarder(address forwarder) public override view returns(bool) {
         return forwarder == trustedForwarder;
     }
 
-    function _msgSender() internal view virtual override returns (address sender) {
-        if (isTrustedForwarder(msg.sender)) {
-            assembly { 
-                sender := shr(96, calldataload(sub(calldatasize(), 20))) 
+    /**
+     * return the sender of this call.
+     * if the call came through our trusted forwarder, return the original sender.
+     * otherwise, return `msg.sender`.
+     * should be used in the contract anywhere instead of msg.sender
+     */
+    function _msgSender() internal override virtual view returns (address ret) {
+        if (msg.data.length >= 20 && isTrustedForwarder(msg.sender)) {
+            // At this point we know that the sender is a trusted forwarder,
+            // so we trust that the last bytes of msg.data are the verified sender address.
+            // extract sender address from the end of msg.data
+            assembly {
+                ret := shr(96,calldataload(sub(calldatasize(),20)))
             }
         } else {
-            return _msgSender();
+            return msg.sender;
         }
     }
-    
+
+    /**
+     * return the msg.data of this call.
+     * if the call came through our trusted forwarder, then the real sender was appended as the last 20 bytes
+     * of the msg.data - so this method will strip those 20 bytes off.
+     * otherwise, return `msg.data`
+     * should be used in the contract instead of msg.data, where the difference matters (e.g. when explicitly
+     * signing or hashing the
+     */
+    function _msgData() internal virtual view returns (bytes memory ret) {
+        if (msg.data.length >= 20 && isTrustedForwarder(msg.sender)) {
+            return msg.data[0:msg.data.length-20];
+        } else {
+            return msg.data;
+        }
+    }
 }
